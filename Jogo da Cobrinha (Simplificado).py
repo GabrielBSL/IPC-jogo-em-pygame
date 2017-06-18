9970be5db1d630b08d44eae2c34b6ac4bd4d1111
Gabriel Barroso <gabriel.barroso.am@gmail.com>
2017-06-17 20:45:38 -0400

    Add files via upload

--- Jogo da Cobrinha (Simplificado).py
+++ Jogo da Cobrinha (Simplificado).py
@@ -1,4 +1,4 @@
-import pygame, sys, os
+﻿import pygame, sys, os
 from pygame.locals import *
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
@@ -18,21 +18,27 @@ pygame.font.init()
 
 time_moviment = 0
 velocity_moviment = 500
+size = 3
 
 tela.fill(WHITE)
 
 class snake(pygame.sprite.Sprite):
 
+
+
     def __init__(self):
 
+        time = 0
+
         pygame.sprite.Sprite.__init__(self)
-        self.ImageSnake = pygame.image.load('images/head.png')
+        self.ImageSnake = pygame.image.load('images2/corpo.png')
 
         self.rect = self.ImageSnake.get_rect()
         self.rect.centerx = weight / 2
         self.rect.centery = height / 2
 
         self.size = 90
+        self.velocity = 20
 
         self.life = True
 
@@ -42,11 +48,20 @@ class snake(pygame.sprite.Sprite):
 
 def SnakeGame():
 
+    jogador = [snake(), snake(), snake()]
     player = snake()
+
     jogando = True
     direction = 1 # 1 - right || 2 - left || 3 - up || 4 - down
 
-    while True:
+    pygame.mixer.music.load("Sounds/Game_Sound_2.wav")
+    pygame.mixer.music.set_volume(1)
+    pygame.mixer.music.play(0)
+
+    while jogando == True:
+
+        print("teste")
+        pygame.time.delay(velocity_moviment)
 
         for event in pygame.event.get():
 
@@ -57,25 +72,39 @@ def SnakeGame():
 
             if event.type == pygame.KEYDOWN:
 
-                if event.key == K_RIGHT:
+                if event.key == K_RIGHT and direction != 2:
 
                     direction = 1
 
-                if event.key == K_LEFT:
+                if event.key == K_LEFT and direction != 1:
 
                     direction = 2
 
-                if event.key == K_UP:
+                if event.key == K_UP and direction != 4:
 
                     direction = 3
 
-                if event.key == K_DOWN:
+                if event.key == K_DOWN and direction != 3:
 
                     direction = 4
 
-        player.put(tela)
+        if direction == 1:
 
-        pygame.time.delay(velocity_moviment)
+            player.rect.right += player.velocity
+
+        elif direction == 2:
+
+            player.rect.left -= player.velocity
+
+        elif direction == 3:
+
+            player.rect.y -= player.velocity
+
+        else:
+
+            player.rect.y += player.velocity
+
+        player.put(tela)
 
         pygame.display.update()
 
